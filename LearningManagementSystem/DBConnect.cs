cc1d491225c8afa95b046e872d552b1cd425015c
Matthew Yackel <mattyackel@hotmail.com>
2023-04-21 23:48:09 -0600

    Adds the DBConnect class to connect to the database

---
+++ LearningManagementSystem/DBConnect.cs
@@ -0,0 +1,194 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// view courses (list view of all coureses who belong to X program
+// Update course select a course from the list. Populate text boxes with current values
+// can change the values in the box then updateButton to save to db
+
+namespace LearningManagementSystem
+{
+    internal class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Initializes DB values
+        /// </summary>
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "studentmanagementsystem";
+            uid = "root";
+            password = "password";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        /// <summary>
+        /// Open the connection to the database
+        /// </summary>
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Closes the connection to the database
+        /// </summary>
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Insert new student object into the students table in the database
+        /// </summary>
+        /// <param name="newStudent">Student object</param>
+        public void InsertNewStudent(Student newStudent)
+        {
+            string query = $"INSERT INTO students VALUES ({newStudent.Id}, '{newStudent.FirstName}', '{newStudent.LastName}', '{newStudent.Program}', '{newStudent.Phone}', '{newStudent.Email}')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// WORK IN PROGRESS - Updates data in the courses table in the database.
+        /// </summary>
+        public void UpdateCourse(int courseId, string courseName, string instructor, DateTime startDate, DateTime endDate)
+        {
+            string query = $"UPDATE courses SET courseName='{courseName}', instructor='{instructor}', startDate='{startDate}', endDate='{endDate}' WHERE courseId='{courseId}'";
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// NOT YET IMPLEMENTED - Delete values from the database
+        /// </summary>
+        public void Delete()
+        {
+            string query = "DELETE FROM tableinfo WHERE name='John Smith'";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// NOT YET IMPLEMENTED - Display information from the database
+        /// </summary>
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM tableinfo";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["age"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+    }
+}
+
